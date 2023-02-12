4f1aaa338b43ccd5bedc7a7675567be906f24a06
Vasilisa Marachkouskaya <vasilisa_marachkouskaya@epam.com>
2023-02-12 21:25:45 +0100

    add microservices

---
+++ microservices/micro-collector/src/main/java/org/example/web/CollectorController.java
@@ -0,0 +1,9 @@
+package org.example.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CollectorController {
+
+}
---
+++ microservices/micro-sender/src/main/java/org/example/app/Notification.java
@@ -0,0 +1,9 @@
+package org.example.app;
+
+import lombok.Data;
+
+@Data
+public class Notification {
+    private String userName;
+    private String message;
+}
