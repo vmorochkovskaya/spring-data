a622f10d47b7c724395286278f44d6bce6ab55cd
Vasilisa Morochkovskaya <vasilisademuanova2700@mail.ru>
2024-01-11 14:39:40 +0100

    Delete task3-spring-testing directory

--- task3-spring-testing/src/main/java/org/example/app/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.repository;
-
-import org.example.app.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Ticket, Integer> {
-    Ticket save(Ticket ticket);
-    Ticket findTicketByPlace(Integer place);
-}
--- task3-spring-testing/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into event (id, title, date) VALUES (1, 'Event-1', '2023-02-02');
-insert into event (id, title, date) VALUES (2, 'Event-2', '2023-02-03');
-
-insert into user_entity (id, email) VALUES (1, 'test@gmail.coom');
-insert into user_entity (id, email) VALUES (2, 'harry@gmail.coom');
